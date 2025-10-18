53b6d99b553c1db051133e2c1d28e71ef6c3de37
Tymofii Sova <timsova2004@gmail.com>
2025-10-18 19:21:05 +0200

    Добавленна анимация для settingContaner  (методы showSettings, hideSettings)

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -7,10 +7,16 @@ export default class MainMenuScene extends Phaser.Scene {
     create() {
         const bg = this.add.image(0, 0, 'bg').setOrigin(0, 0);
         const fullScreen = this.add.image(1490, 0, 'fullScreen').setOrigin(0, 0).setScale(0.05).setTintFill(0xffffff);
-        this.settingsContaner = this.add.container(1200,100).setVisible(false);
+        this.settingsContaner = this.add.container(1200, 100).setVisible(false);
         let settingsBackground = this.add.rectangle(0, 0, 300, 300, 0x000000, 0.8).setOrigin(0);
         this.settingsContaner.add(settingsBackground);
-        this.closeBtn = this.add.text(1480,100,'X', {fontSize: '32px', fill: '#fff'}).setInteractive({useHandCursor: true}).setVisible(false);
+        this.closeBtn = this.add.text(1480, 100, 'X', { fontSize: '32px', fill: '#fff' }).setInteractive({ useHandCursor: true }).setVisible(false);
+        
+
+
+
+
+
         WebFont.load({
             google: {
                 families: ['Outfit']
@@ -30,17 +36,40 @@ export default class MainMenuScene extends Phaser.Scene {
                     fontFamily: 'Outfit',
                     fontSize: '32px',
                     color: '#fff'
-                }).setInteractive({useHandCursor: true});
-                this.settings.on('pointerdown', ()=>{
-                    this.settingsContaner.setVisible(true);
-                    this.closeBtn.setVisible(true);
+                }).setInteractive({ useHandCursor: true });
+                this.settings.on('pointerdown', () => {
+                    this.showSettings();
                 })
             }
         });
 
-        this.closeBtn.on('pointerdown', ()=>{
-            this.settingsContaner.setVisible(false);
+        this.closeBtn.on('pointerdown', () => {
+            this.hideSettings();
             this.closeBtn.setVisible(false);
+        });
+    }
+
+    showSettings() {
+        this.closeBtn.setVisible(true);
+        this.settingsContaner.setVisible(true);
+        this.tweens.add({
+            targets: this.settingsContaner,
+            alpha: 1,
+            scale: 1,
+            duration: 300,
+            ease: 'Back.Out'
+        });
+    }
+    hideSettings() {
+        this.tweens.add({
+            targets: this.settingsContaner,
+            alpha: 0,
+            scale: 0,
+            duration: 200,
+            ease: 'Back.In',
+             onComplete: () => {
+                this.settingsContaner.setVisible(false);
+            }
         });
     }
 }
