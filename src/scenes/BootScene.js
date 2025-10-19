6a9840b12c51da491f8aeb940826eed414fd8d4d
ADONYARIK <scityrchenko@gmail.com>
2025-10-19 18:19:43 +0200

    Refactor BootScene and GameScene: Comment out unused asset loading and lighting code for future implementation

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -26,5 +26,12 @@ export default class BootScene extends Phaser.Scene {
         this.load.image('settingsIcon', './src/assets/newSprites/settingsIcon.png');
         this.load.atlas('game', './src/assets/atlas/spritesheet.png', './src/assets/atlas/spritesheet.json');
         this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+
+        // Атлас спрайтов + нормали
+        // this.load.atlas("sprites", "assets/atlas/spritesheet.png", "assets/atlas/spritesheet.json");
+        // this.load.image("sprites_n", "assets/atlas/spritesheet_n.png");
+
+        // Коллайдеры
+        // this.load.json("shapes", "assets/physics/shapes.json");
     }
 }
