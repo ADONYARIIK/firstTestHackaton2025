a1b7e28366875101c3a1f9e5ef8b6a2628a52098
ADONYARIK <scityrchenko@gmail.com>
2025-10-24 22:23:32 +0200

    kabzda

--- src/assets/maps/tilesets/test/tileset_decor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="decor" tilewidth="70" tileheight="70" tilecount="49" columns="7">
- <image source="../tileset_decor.png" width="490" height="490"/>
-</tileset>
--- src/assets/maps/tilesets/tileset_usable.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="usable" tilewidth="70" tileheight="70" tilecount="72" columns="9">
- <image source="tileset_usable.png" width="630" height="560"/>
-</tileset>
