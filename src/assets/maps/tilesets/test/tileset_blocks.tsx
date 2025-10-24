ba8b6b9219bf70461a59409b9784b7cb6f6263ca
ADONYARIIK <scityrchenko@gmail.com>
2025-10-24 22:22:02 +0200

    Delete src/assets directory

--- src/assets/maps/tilesets/test/tileset_blocks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="blocks" tilewidth="70" tileheight="70" tilecount="256" columns="16">
- <image source="../tileset_blocks.png" width="1120" height="1120"/>
-</tileset>
--- src/assets/maps/tilesets/tileset_enemies.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="enemies" tilewidth="53" tileheight="145" tilecount="12" columns="6">
- <image source="tileset_enemies.png" width="349" height="316"/>
-</tileset>
