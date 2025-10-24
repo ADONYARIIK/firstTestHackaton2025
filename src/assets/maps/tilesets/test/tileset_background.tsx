ca3255c51f7211b3a40a2d6ae1f8731277c8cb01
ADONYARIK <scityrchenko@gmail.com>
2025-10-24 23:22:00 +0200

    aboba6

---
+++ src/assets/maps/tilesets/test/tileset_background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="background" tilewidth="70" tileheight="70" tilecount="98" columns="14">
+ <image source="../../../sprites/level1/background/bg_castle.png" width="1024" height="512"/>
+</tileset>
