ab7fd206ecf8eda1c1a438acfcc3c05de1756040
ADONYARIK <scityrchenko@gmail.com>
2025-10-26 01:15:31 +0200

    Добавление не достающих спрайтов, удаление не нужных файлов,  полное подключение карты тайлов из tiled, не большая оптимизация и перепись кода, осталось разобратся с подключением слоёв объектов из Tiled.

--- src/utils/lightHelper.js
+++ src/utils/lightHelper.js
@@ -1,9 +1,42 @@
 export function setupLights(scene) {
-    scene.lights.enable().setAmbientColor(0x222222);
+    // scene.lights.enable().setAmbientColor(0xf0f0f0);
+    scene.lights.enable().setAmbientColor(0x000);
 
-    scene.lights.addLight(100, 100, 10000).setIntensity(2.5);
-    scene.lights.addLight(100, 1000, 10000, 0xda711b).setIntensity(1.5);
+    createTorchLight(scene, 105, 1000);
+    createLavaLight(scene, 390, 1200);
 
-    // scene.lights.addLight(100, 1600, 10000).setColor(0xda711b).setIntensity(2.5);
-    // scene.lights.addLignt(x, y, radius, color, intensity);
+    // scene.lights.addLight(100, 100, 10000).setIntensity(2.5);
+    // scene.lights.addLight(100, 1000, 10000, 0xda711b).setIntensity(1.5);
+}
+
+function createTorchLight(scene, x, y) {
+    const light = scene.lights.addLight(x, y, 5000, 0xffaa33, 1.5);
+
+    scene.tweens.add({
+        targets: light,
+        radius: { from: 4500, to: 5500 },
+        intensity: { from: 1.4, to: 1.6 },
+        duration: 2000,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut'
+    });
+
+    return light;
+}
+
+function createLavaLight(scene, x, y, width = 300) {
+    const light = scene.lights.addLight(x, y, width*0.8, 0xff3300, 1);
+
+    scene.tweens.add({
+        targets: light,
+        radius: { from: width * 0.7, to: width * 0.9 },
+        intensity: { from: 0.9, to: 1.1 },
+        duration: 2000,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut'
+    });
+
+    return light;
 }
