62047e9c819f0864ed2cd22540a2c11a64b72ca1
ADONYARIK <scityrchenko@gmail.com>
2025-10-26 23:26:47 +0100

    Удалены нормалмапы в связи с отказом от них, исправленно добавление слоя объектов из tiled, добавляется вся карта tiled, добавленно управление персонажем(очень плохое, работать и работать), изменены настройки создания света.

--- src/game.js
+++ src/game.js
@@ -10,7 +10,7 @@ export const config = {
     type: Phaser.AUTO,
     width: 1600,
     height: 900,
-    backgroundColor: '#ba6aa7',
+    backgroundColor: '0xba6aa7',
     parent: 'game-container',
     physics: {
         default: 'matter',
--- src/utils/lightHelper.js
+++ src/utils/lightHelper.js
@@ -1,39 +1,71 @@
-export function setupLights(scene) {
-    // scene.lights.enable().setAmbientColor(0xf0f0f0);
+export function setupLights(scene, map) {
     scene.lights.enable().setAmbientColor(0x000);
 
-    createTorchLight(scene, 105, 1000);
-    createLavaLight(scene, 390, 1200);
+    const lightsLayer = map.getObjectLayer('Lights').objects;
+    const lights = {};
 
-    // scene.lights.addLight(100, 100, 10000).setIntensity(2.5);
-    // scene.lights.addLight(100, 1000, 10000, 0xda711b).setIntensity(1.5);
+    lightsLayer.forEach(obj => {
+        const props = (obj.properties || []).reduce((acc, prop) => {
+            acc[prop.name] = prop.value;
+            return acc;
+        }, {});
+
+        let light;
+
+        if (obj.name === 'Torch') {
+            light = createTorchLight(scene, obj.x + obj.width / 2, obj.y + obj.height / 2);
+        } else if (obj.name === 'Lava') {
+            light = createLavaLight(scene, obj.x + obj.width / 2, obj.y + obj.height / 1, obj.width);
+        }
+
+        lights[obj.name] = light;
+    });
+
+    return lights;
 }
 
 function createTorchLight(scene, x, y) {
-    const light = scene.lights.addLight(x, y, 5000, 0xffaa33, 1.5);
+    const light = scene.lights.addLight(x, y, 2000, 0xeb750e, 0.6);
+    // const light = scene.lights.addLight(x, y, 2000, 0xe36f09, 0.6);
+    // const light = scene.lights.addLight(x, y, 2000, 0xff8b33, 0.6);
+
+    const duration = Phaser.Math.Between(1000, 3000);
+    const delay = Phaser.Math.Between(0, 1000);
 
     scene.tweens.add({
         targets: light,
-        radius: { from: 4500, to: 5500 },
-        intensity: { from: 1.4, to: 1.6 },
-        duration: 2000,
-        yoyo: true,
+        radius: { from: 1800, to: 2200 },
+        intensity: { from: 0.5, to: 0.7 },
+        duration: duration,
+        yoyo: delay,
         repeat: -1,
         ease: 'Sine.easeInOut'
+    });
+
+    scene.time.addEvent({
+        delay: Phaser.Math.Between(100, 300),
+        loop: true,
+        callback: () => {
+            light.intensity = Phaser.Math.FloatBetween(0.4, 0.7);
+            light.radius = Phaser.Math.Between(1800, 2200);
+        }
     });
 
     return light;
 }
 
 function createLavaLight(scene, x, y, width = 300) {
-    const light = scene.lights.addLight(x, y, width*0.8, 0xff3300, 1);
+    const light = scene.lights.addLight(x, y, width * 0.4, 0xff3300, 0.5);
+
+    const duration = Phaser.Math.Between(1500, 4000);
+    const delay = Phaser.Math.Between(0, 1000);
 
     scene.tweens.add({
         targets: light,
-        radius: { from: width * 0.7, to: width * 0.9 },
-        intensity: { from: 0.9, to: 1.1 },
-        duration: 2000,
-        yoyo: true,
+        radius: { from: width * 0.35, to: width * 0.45 },
+        intensity: { from: 0.45, to: 0.55 },
+        duration: duration,
+        yoyo: delay,
         repeat: -1,
         ease: 'Sine.easeInOut'
     });
